b9cf7c77108b1d1a9d64d2fd47dde9efac06171e
ZhinHxH <jhosben16@gmail.com>
2024-12-19 14:05:03 -0500

    feat:sparkles: Adicion de los metodos generales CRUD para las vistas moviles

--- frontOI/client_OI/src/hook/services/apiService.js
+++ frontOI/client_OI/src/hook/services/apiService.js
@@ -1,5 +1,75 @@
 import axios from 'axios';
-const baseUrl = 'https://2647-181-60-112-158.ngrok-free.app/api/';
+const baseUrl = 'http://localhost:8000/api/';
+
+// Configuracion comun para las solicitudes para los headers y la informacion por fuera del payload
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true', // Evitar advertencia de ngrok en el navegador
+  },
+});
+
+
+// Servicio general para obtener datos (GET) de todas las estructuras de datos
+export const getAll = async (endpoint, params = {}) => {
+  try {
+    const queryString = new URLSearchParams(params).toString();
+    const response = await axiosInstance.get(`${endpoint}${queryString ? `?${queryString}` : ''}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error en Get todos: ', error);
+    throw error.response ? error.response.data : 'Network Error';
+  }
+};
+
+// Servicio general para obtener datos por id para todas las estructuras de datos
+export const getByKey = async (endpoint, key) => {
+  try {
+    const response = await axiosInstance.get(`${endpoint}/${key}`)
+  } catch (error) {
+    console.error('Error ocurrido en Get by key: ', error)
+    throw error.response ? error.response.data : 'Network Error';
+  }
+};
+
+// Servicio general de creacion de estructuras de datos post
+export const create = async (endpoint, data) => {
+  try {
+    const response = await axiosInstance.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating data:', error);
+    throw error.response ? error.response.data : 'Network Error';
+  }
+};
+
+// Servicio general para actualizar registros
+export const updateData = async (endpoint, data, method = 'PUT') => {
+  try {
+    const response = await axiosInstance({
+      method: method.toUpperCase(),
+      url: endpoint,
+      data,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error updating data:', error);
+    throw error.response ? error.response.data : 'Network Error';
+  }
+};
+
+// Servicio general para eliminar un registro
+export const deleteData = async (endpoint, key) => {
+  try {
+    const response = await axiosInstance.delete(`${endpoint}/${key}/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting data:', error);
+    throw error.response ? error.response.data : 'Network Error';
+  }
+};
+
 
 //servicio para hacerle get a los valores de los medidores
 const getOrdenes = async (params) => {
@@ -67,9 +137,14 @@ export const postMeters = async (data) => {
   }
 };
 
-
 export default {
+  getAll,
+  getByKey,
+  create,
+  updateData,
+  deleteData,
   getOrdenes,
   getMedidoresPrueba,
-  updateMetersPrueba, 
+  updateMetersPrueba,
+  postMeters,
 };
